405df8bb5a141097d65570fb10c04cd4dfd225c5
Jason Scott <jason@jaylin.com.au>
2014-05-07 12:10:19 +0800

    add a file receiver step to the route

--- src/main/java/camelcoredemo/TimerBeansMain.java
+++ src/main/java/camelcoredemo/TimerBeansMain.java
@@ -1,15 +1,26 @@
 package camelcoredemo;
 
-import org.slf4j.*;
-import org.apache.camel.*;
-import org.apache.camel.builder.*;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.main.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
+/**
+ * Test Apache Camel
+ * This basic program can be executed from the command line with maven: mvn compile exec:java
+ * and will setup a route with a timer (every 5 sec) sending to a bean and then a file
+ * receiver.
+ * 
+ * Note: the file processor path is relevant to the current diretory.
+ */
 public class TimerBeansMain extends Main {
     static Logger LOG = LoggerFactory.getLogger(TimerBeansMain.class);
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String... args) throws Exception {
         TimerBeansMain main = new TimerBeansMain();
         main.enableHangupSupport();
         main.bind("processByBean1", new Bean1());
@@ -21,9 +32,10 @@ public class TimerBeansMain extends Main {
     static RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
                 public void configure() {
-                    from("timer://timer1?period=1000")
+                    from("timer://timer1?period=5000")
                     .to("bean:processByBean1")
-                    .to("bean:processAgainByBean2");
+                    //.to("bean:processAgainByBean2")
+                    .to("file://test_file_output?fileName=mydata-${date:now:yyyyMMdd-HHmmss}.txt");
                 }
             };
     }
@@ -31,6 +43,9 @@ public class TimerBeansMain extends Main {
     // Processor beans
     static class Bean1 implements Processor {
         public void process(Exchange msg) {
+            Message m = msg.getOut();
+            m.setBody("this is some message body data");
+
             LOG.info("First process {}", msg);
         }
     }
